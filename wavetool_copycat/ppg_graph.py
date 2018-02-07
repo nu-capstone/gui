17b6e8e7cbf075470aa44726176ac74e88a04cd2
tomqhoang <tomqhoang@github.com>
2018-02-07 12:42:46 -0500

    minor changes to graph viewer

--- wavetool_copycat/ppg_graph.py
+++ wavetool_copycat/ppg_graph.py
@@ -1,33 +1,30 @@
-#---------Imports
-from numpy import arange, sin, pi
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+#!/usr/bin/env python
 import Tkinter as tk
+from PIL import Image, ImageTk
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-#---------End of imports
+matplotlib.use("TkAgg")
+from numpy import arange, sin, pi
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 
-class ppg_graph(tk.Frame):
+class ppg_graph:
+	def __init__(self, master):
+		tk.Frame.__init__(self, master)
+		fig = plt.Figure()
+		x = np.arange(0, 2*np.pi, 0.01)        # x-array
+		canvas = FigureCanvasTkAgg(fig, master=master)
+		canvas.get_tk_widget().grid(column=0,row=1)
 
-    def __init__(self, master, controller):
-        tk.Frame.__init__(self, master)
-        label = tk.Label(master,text="SHM Simulation").grid(column=0, row=0)
+		ax = fig.add_subplot(111)
+		line, = ax.plot(x, np.sin(x))
+		ani = animation.FuncAnimation(fig, self.animate, np.arange(1, 200), interval=25, blit=False)
 
-        button2 = tk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(ppg_highlevel))
-        button2.grid(row = 0, column = 0)
+	def animate(i):
+		line.set_ydata(np.sin(x+i/10.0))  # update the data
+		return line,
 
-        fig = plt.Figure()
-        x = np.arange(0, 2*np.pi, 0.01)        # x-array
-        master = tk.Tk()
-        canvas = FigureCanvasTkAgg(fig, master=master)
-        canvas.get_tk_widget().grid(column=0,row=1)
-
-        ax = fig.add_subplot(111)
-        line, = ax.plot(x, np.sin(x))
-        ani = animation.FuncAnimation(fig, self.animate, np.arange(1, 200), interval=25, blit=False)
-
-    def animate(i):
-        line.set_ydata(np.sin(x+i/10.0))  # update the data
-        return line,
+	def show(self):
+		self.lift()
